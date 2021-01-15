f5c46d583c60d6c46f2c9170abf57943578c1f6c
Archie Hurst <HUR202000661@longroad.ac.uk>
2021-01-15 16:02:29 +0000

    upload to github

--- Hokemon/Program.cs
+++ Hokemon/Program.cs
@@ -8,76 +8,106 @@ namespace Hokemon
         {
             Hokemon hokemon1 = new Hokemon();
             Hokemon hokemon2 = new Hokemon();
-        }
-    }
-    public class Hokemon
-    {
-        public Hokemon() //constuctor
-        {
-            void getName()
+            battleArena battleArena1 = new battleArena();
+            if (battleArena1.battleRequest(hokemon1, hokemon2) == true)
             {
-                Console.WriteLine("Enter a name for your Hokemon");
+                battleArena1.fight(hokemon1, hokemon2)
+            }
+        }
+        public class Hokemon
+        {
+            public Hokemon() //constuctor
+            {
+                void getName()
+                {
+                    Console.WriteLine("Enter a name for your Hokemon");
 
-                Name = Console.ReadLine();
+                    Name = Console.ReadLine();
+                }
+                void getDetails()
+                {
+                    Console.WriteLine("Name: {0}\nHealth: {1}/{2}\nAttack: {3}\nDefense: {4}\nSpeed: {5}\n", name, health, maxHealth, attack, defense, speed);
+                }
+                int random_int_generator(int minValue, int maxValue)
+                {
+                    Random rnd = new Random();
+                    int randomValue;
+                    randomValue = rnd.Next(minValue, maxValue);
+                    return randomValue;
+                }
+                getName();
+                maxHealth = 100;
+                health = maxHealth;
+                //health = random_int_generator(10, maxHealth);
+                attack = random_int_generator(10, 100);
+                speed = random_int_generator(10, 100);
+                defense = random_int_generator(10, 100);
+                getDetails();
             }
-            void getDetails()
+            private string name;
+            private int health;
+            private int attack;
+            private int maxHealth;
+            private int speed;
+            private int defense;
+            public string Name
             {
-                Console.WriteLine("Name: {0}\nHealth: {1}/{2}\nAttack: {3}\nDefense: {4}\nSpeed: {5}\n",name, health, maxHealth, attack, defense, speed);
+                get { return name; }
+                set { name = value; }
             }
-            int random_int_generator(int minValue, int maxValue)
+            public int Health
             {
-                Random rnd = new Random();
-                int randomValue;
-                randomValue = rnd.Next(minValue, maxValue);
-                return randomValue;
+                get { return health; }
+                set { health = value; }
             }
-            getName();
-            maxHealth = 100;
-            health = maxHealth;
-            //health = random_int_generator(10, maxHealth);
-            attack = random_int_generator(10, 100);
-            speed = random_int_generator(10, 100);
-            defense = random_int_generator(10, 100);
-            getDetails();
+            public int Attack
+            {
+                get { return attack; }
+                set { attack = value; }
+            }
+            public int MaxHealth
+            {
+                get { return maxHealth; }
+                set { maxHealth = value; }
+            }
+            public int Speed
+            {
+                get { return speed; }
+                set { speed = value; }
+            }
+            public int Defense
+            {
+                get { return defense; }
+                set { defense = value; }
+            }
         }
-        private string name;
-        private int health;
-        private int attack;
-        private int maxHealth;
-        private int speed;
-        private int defense;
-        public string Name
+        public class battleArena
         {
-            get { return  name; }
-            set { name = value;  }
-        }
-        public int Health
-        {
-            get { return health; }
-            set { health = value; }
-        }
-        public int Attack
-        {
-            get { return attack; }
-            set { attack = value; }
-        }
-        public int MaxHealth
-        {
-            get { return maxHealth; }
-            set { maxHealth = value; }
-        }
-        public int Speed
-        {
-            get { return speed; }
-            set { speed = value; }
-        }
-        public int Defense
-        {
-            get { return defense; }
-            set { defense = value; }
+            public battleArena() //constructor
+            {
+
+            }
+            public void fight(Hokemon hokemon1, Hokemon hokemon2)
+            {
+                Console.WriteLine("it is {0}'s turn ")
+            }
+            public bool battleRequest(Hokemon challenger1, Hokemon challenger2)
+            {
+                string request;
+                Console.WriteLine("{0} wants to fight {1}. do you accept?", challenger1, challenger2);
+                request = Console.ReadLine();
+                if (request.ToUpper() == "YES")
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
         }
     }
-    
+
 
 
 }
